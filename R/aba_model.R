f1f22116d7d1370fb5f220fe0e504a0bd0e6446d
ncullen93 <nicholas.cullen@med.lu.se>
2021-10-18 19:40:28 +0200

    commit

--- R/aba_model.R
+++ R/aba_model.R
@@ -1,4 +1,24 @@
 
+
+
+#' Create an aba model
+#'
+#' @return abaModel
+#' @export
+#'
+#' @examples
+#' m <- aba_model()
 aba_model <- function() {
 
+  m <- list(
+    'data' = list(),
+    'spec' = list(),
+    'fits' = list()
+  )
+
+  class(m) <- 'abaModel'
+
+  return(
+    m
+  )
 }
