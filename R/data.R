0eba7f42725ca27ccc577daa8a52e9881ffbd5e4
ncullen93 <nicholas.cullen@med.lu.se>
2021-10-20 11:10:49 +0200

    added data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' A sample of ADNI data in long format
+#'
+#' A sample of ADNI data in long format. This data contains longitudinal
+#' follow-up visits from CU and MCI patients, with baseline measures of
+#' plasma biomarkers (abeta, p-tau, nfl), CSF biomarkers, MRI measures,
+#' cognition, and demographics. Longitudinal outcomes include cognition
+#' (MMSE, CDRSB) and conversion to AD dementia and conversion to
+#' all-cause dementia. There is also binarized biomarker status for
+#' many of the variables.
+#'
+#' @format A data frame with 1651 rows and 31 variables.
+#'
+#' @source \url{adni.loni.usc.edu}
+"adni_sample"
