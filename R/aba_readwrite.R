f3293c0f5f8bb108aab6f61da668f89ce3ab34e7
ncullen93 <nicholas.cullen@med.lu.se>
2021-11-29 14:29:42 +0100

    readwrite

--- R/aba_readwrite.R
+++ R/aba_readwrite.R
@@ -1,5 +1,3 @@
-
-
 #' Write an ABA object to file
 #'
 #' @param object ABA object
@@ -14,29 +12,74 @@
 #' @examples
 #' x <- 1
 aba_write <- function(object,
-                      file,
-                      name = NULL,
-                      include_data = TRUE,
-                      include_fit = TRUE) {
-
-  if (!include_data & ('abaModel' %in% class(object))) {
-    object$data <- NULL
-  }
-  if (!include_fit & ('abaModel' %in% class(object))) {
-    stat_names <- names(object$spec$stats)
-    object$results[,stat_names] <- NULL
-  }
-
-  if ('abaBoard' %in% class(file)) {
-    board <- file$board
-    if (is.null(name)) name <- 'abaModel'
-    board %>% pins::pin_write(object, name = name, type = 'rds')
-  } else {
-
-    saveRDS(object = object, file = file)
-  }
-
+                      filename,
+                      ...) {
+  UseMethod('aba_write')
 }
+
+#' Write an aba summary object to file
+#' @export
+aba_write.abaSummary <- function(object,
+                                 filename,
+                                 ...) {
+  r <- object$results
+  if (endsWith(filename, '.csv')) {
+    r %>%
+      write.csv(
+        filename,
+        row.names=F
+      )
+  } else if (endsWith(filename, '.xlsx')) {
+    r %>%
+      writexl::write_xlsx(filename)
+  } else {
+    stop('Filename must end in .csv or .xlsx')
+  }
+}
+
+#' Write an aba demo table (TableOne) to file
+#' @export
+aba_write.TableOne <- function(object,
+                               filename,
+                               ...) {
+  r <- object
+
+  if (endsWith(filename, '.csv')) {
+    write.csv(
+      print(r, showAllLevels=T),
+      filename,
+      fileEncoding = 'UTF-8'
+    )
+  } else {
+    stop('Filename must end in .csv')
+  }
+}
+
+
+#aba_write.abaModel <- function(object,
+#                      file,
+#                      name = NULL,
+#                      include_data = TRUE,
+#                      include_fit = TRUE) {
+#
+#  if (!include_data & ('abaModel' %in% class(object))) {
+#    object$data <- NULL
+#  }
+#  if (!include_fit & ('abaModel' %in% class(object))) {
+#    stat_names <- names(object$spec$stats)
+#    object$results[,stat_names] <- NULL
+#  }
+#
+#  if ('abaBoard' %in% class(file)) {
+#    board <- file$board
+#    if (is.null(name)) name <- 'abaModel'
+#    board %>% pins::pin_write(object, name = name, type = 'rds')
+#  } else {
+#
+#    saveRDS(object = object, file = file)
+#  }
+#
+#}
 
 
 #' Read an ABA object from file
