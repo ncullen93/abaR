afbbb11465805a4ecc008188870f16d2349b7e7f
ncullen93 <nicholas.cullen@med.lu.se>
2021-12-13 21:28:06 +0100

    tests

--- tests/testthat/test-stat_mmrm.R
+++ tests/testthat/test-stat_mmrm.R
@@ -1,28 +1,37 @@
 test_that("standard mmrm works", {
 
   # fit
+  df <- adnimerge %>%
+    dplyr::filter(
+      VISCODE %in% c('bl','m06','m12','m24'),
+      !is.na(CSF_ABETA_STATUS_bl),
+      DX_bl %in% c('MCI')
+    ) %>%
+    dplyr::mutate(
+      TREATMENT = factor(CSF_ABETA_STATUS_bl, levels=c(0,1),
+                         labels=c('Placebo','Treatment')),
+      ADAS13 = ADAS13 - ADAS13_bl,
+      CDRSB = CDRSB - CDRSB_bl,
+      MMSE = MMSE - MMSE_bl
+    )
+
+  # fit mmrm model for different endpoints, adjusted for covariates
   expect_error(
-    model <- adnimerge %>%
-      dplyr::filter(
-        VISCODE %in% c('bl', 'm06', 'm12', 'm24')
+    model <- df %>% aba_model() %>%
+      set_outcomes(CDRSB, ADAS13, MMSE) %>%
+      set_covariates(
+        AGE, GENDER, EDUCATION
       ) %>%
-      aba_model() %>%
-      set_groups(DX_bl == 'MCI', everyone()) %>%
-      set_outcomes(MMSE, CDRSB) %>%
-      set_predictors(
-        PLASMA_ABETA_bl,
-        PLASMA_PTAU181_bl,
-        PLASMA_NFL_bl,
-        c(PLASMA_ABETA_bl, PLASMA_PTAU181_bl, PLASMA_NFL_bl)
-      ) %>%
-      set_covariates(AGE, GENDER, EDUCATION) %>%
       set_stats(
-        stat_mmrm(id='RID', time='VISCODE')
+        stat_mmrm(id = 'RID', time = 'VISCODE', treatment = 'TREATMENT')
       ) %>%
-      aba_fit() %>%
-      aba_summary(),
+      aba_fit(),
     NA
   )
 
+  expect_error(
+    model_summary <- model %>% aba_summary(),
+    NA
+  )
 
 })
