ff03c6ba2cb14f008147e097fb00d8500cc3074f
ncullen93 <nicholas.cullen@med.lu.se>
2021-12-13 21:34:50 +0100

    test

--- tests/testthat/test-aba_readwrite.R
+++ tests/testthat/test-aba_readwrite.R
@@ -1,3 +1,55 @@
+test_that("example works", {
+  # create temp files to save to
+  tmp_filename_csv <- tempfile(fileext = '.csv')
+  tmp_filename_rda <- tempfile(fileext = '.Rda')
+
+  # grab built-in data
+  data <- adnimerge %>% dplyr::filter(VISCODE == 'bl')
+
+  # fit model
+  model <- data %>% aba_model() %>%
+    set_groups(everyone()) %>%
+    set_outcomes(ConvertedToAlzheimers, CSF_ABETA_STATUS_bl) %>%
+    set_predictors(
+      PLASMA_ABETA_bl, PLASMA_PTAU181_bl, PLASMA_NFL_bl,
+      c(PLASMA_ABETA_bl, PLASMA_PTAU181_bl, PLASMA_NFL_bl)
+    ) %>%
+    set_stats('glm') %>%
+    fit()
+
+  # summarise model
+  model_summary <- model %>% summary()
+
+  # save model summary to file as table
+  expect_error(
+    model_summary %>% aba_write(tmp_filename_csv),
+    NA
+  )
+
+
+  # save model summary to file as raw long-form results
+  expect_error(
+    model_summary %>% aba_write(tmp_filename_csv, format = 'raw'),
+    NA
+  )
+
+  # save model summary as an object which can be loaded back into memory
+  expect_error(
+    model_summary %>% aba_write(tmp_filename_rda, format = 'object'),
+    NA
+  )
+
+  # load summary back to file to show it works
+  expect_error(
+    model_summary2 <- aba_read(tmp_filename_rda),
+    NA
+  )
+
+  # delete temp files
+  removed <- file.remove(tmp_filename_csv)
+  removed <- file.remove(tmp_filename_rda)
+})
+
 test_that("save and load works", {
   df <- adnimerge %>% filter(VISCODE=='bl')
 
